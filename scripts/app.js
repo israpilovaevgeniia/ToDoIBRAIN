c030908fa79ea1e6b890658a62aad2ad17ba44ee
israpilovaevgeniia <israpilovaevgeniia@gmail.com>
2024-07-15 16:49:19 -0500

    create new progect

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,20 @@
 "use strict";
 
 const HABBIT_KEY = "HABBIT_KET";
+let globalActiveHabbitId; 
+
 
 const page = {
-    menu: document.querySelector(".menu__list")
+    menu: document.querySelector(".menu__list"),
+    header: {
+        h1: document.querySelector(".content__title"),
+        progressPercent: document.querySelector(".progress-percent"),
+        scroll: document.querySelector(".scroll-active"),
+    },
+    content: {
+        habbitDay: document.querySelector("#days"),
+        nextDay: document.querySelector(".habbit__day")
+    }
 };
 
 let habbits = [];
@@ -36,7 +47,7 @@ function rerenderMenu (activeHabbit) {
             element.setAttribute("menu-habbit-id", habbit.id);
             element.classList.add("menu__item");
             element.innerHTML = `<img src="./image/${habbit.icon}.svg" alt="${habbit.name} logo">`
-            element.addEventListener("click", () => rerenderMenu(habbit.id));
+            element.addEventListener("click", () => rerender(habbit.id));
             if(activeHabbit.id === habbit.id) {
                 element.classList.add("menu__item-active");
 
@@ -52,16 +63,97 @@ function rerenderMenu (activeHabbit) {
     }
 }
 
+function renderHeader (activeHabbit) {
+    if(!activeHabbit) return;
+    const progress = activeHabbit.days.length / activeHabbit.target > 1 ? 100 : (activeHabbit.days.length / activeHabbit.target) * 100;
+    page.header.h1.innerText = activeHabbit.name;
+    page.header.progressPercent.innerText = `${progress}%`;
+    page.header.scroll.style.width = `${progress}%`; 
+}
+
+function renderContent (activeHabbit) {
+    if(!activeHabbit) return;
+
+    page.content.habbitDay.innerHTML = "";
+    activeHabbit.days.forEach((day, idx) => {
+        const element = document.createElement("div");
+        element.classList.add("habbit");
+        element.innerHTML = `
+            <div class="habbit__day">День ${idx + 1}</div>
+            <div class="habbit__comment">${day.comment }</div>
+            <button class="form__delete">
+            <img src="./image/delete.svg" alt="delete">
+            </button>
+            `;
+        page.content.habbitDay.appendChild(element);
+        page.content.nextDay.innerHTML = `День ${activeHabbit?.days?.length + 1}`;
+    });
+    if(!activeHabbit.days.length) {
+        page.content.nextDay.innerHTML = `День ${activeHabbit?.days?.length + 1}`;
+    }
+}
+
+function createDay (event) {
+    const form = event.target;
+    event.preventDefault();
+    const data = new FormData(form);
+    const comment = data.get("comment");
+    //Валидация 
+    if(!comment) {
+        form["comment"].classList.add("error");
+    }else {
+        form["comment"].classList.remove("error");
+    }
+
+    //creade day
+    habbits = habbits.map((habbit) => {
+         if (habbit.id === globalActiveHabbitId) {
+            return {
+                ...habbit,
+                days: [...habbit.days, { comment: comment}],
+            }
+         } else {
+            return habbit;
+         }
+    });
+    form["comment"].value = "";
+    setData();
+    rerender(globalActiveHabbitId);
+}
+
+function removeDay (activeHabbit) {
+    if (!activeHabbit) return;
+
+    document.querySelectorAll(".form__delete").forEach((deleteButton, buttonIndex) => {
+        deleteButton.addEventListener("click", () => {
+            const dayIndexToRemove = buttonIndex;
+            const updatedDays = activeHabbit.days.reduce ((acc, day, dayIndex) => {
+                if(dayIndex !== dayIndexToRemove) {
+                    acc.push(day);
+                    return acc;
+                }
+                return acc;
+            }, []); 
+            activeHabbit.days = updatedDays;
+            setData();
+            rerender(activeHabbit.id);   
+        })
+    })
+}
+
 
 function rerender(activeHabbitId) {
+    globalActiveHabbitId = activeHabbitId;
     const selectetHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
     rerenderMenu(selectetHabbit);
-
+    renderHeader(selectetHabbit);
+    renderContent(selectetHabbit);
+    removeDay(selectetHabbit);
 }
 
 
 //init(запуск приложения)
 (() => {
     getData();
-    rerenderMenu(habbits[0]);
+    rerender(habbits[0].id);
 }) ();
